9408b39f8d3beab7958fada45dda9426493e1115
Masood <67869180+MSalman5230@users.noreply.github.com>
2023-09-04 19:43:30 +0530

    feat: Get file Id

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ import time
 from bs4 import BeautifulSoup
 
 url="https://www.techpowerup.com/download/nvidia-dlss-dll/"
-source_code = requests.get(url).text
-soup = BeautifulSoup(source_code, "lxml")
-#elements = soup.find_all(True, {"class": "versions"})
-#elements = soup.find(False, {"class": "version"})
 
-# Find the <form> element
-form = soup.find("form", {"class": "download-version-form"})
 
-# Find the <input> element within the <form> by name
-input_element = form.find("input", {"name": "id"})
+def get_file_id(url):
+    source_code = requests.get(url).text
+    soup = BeautifulSoup(source_code, "lxml")
 
-# Get the value attribute of the <input> element
-value = input_element["value"]
+    # Find the <form> element
+    form = soup.find("form", {"class": "download-version-form"})
 
-print(value)
+    # Find the <input> element within the <form> by name
+    input_element = form.find("input", {"name": "id"})
+
+    # Get the value attribute of the <input> element
+    value = input_element["value"]
+
+    return value
+
+if __name__=="__main__":
+   print( get_file_id(url))
