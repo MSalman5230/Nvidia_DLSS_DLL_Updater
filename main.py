6015f1d251c4a1bc64fb285a83bcc7ca16b0bc88
Masood <67869180+MSalman5230@users.noreply.github.com>
2023-09-05 03:18:30 +0530

    feat: scrape file ids

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ import shutil
 import os
 import time
 from bs4 import BeautifulSoup
+import zipfile
+from dotenv import load_dotenv
+import datetime
 
-url="https://www.techpowerup.com/download/nvidia-dlss-dll/"
+load_dotenv()
+game_paths_list = os.environ.get("GAME_PATH")
+game_paths_list = [path.strip() for path in game_paths_list.split(",")]
+game_paths_list = [path for path in game_paths_list if path]
+print(game_paths_list)
+TPU_DOWNLOAD_SERVER_ID = 14
+urls = {"DLSS": "https://www.techpowerup.com/download/nvidia-dlss-dll/", "DLSS_FG": "https://www.techpowerup.com/download/nvidia-dlss-3-frame-generation-dll/"}
 
 
 def get_file_id(url):
-    source_code = requests.get(url).text
-    soup = BeautifulSoup(source_code, "lxml")
+    try:
+        url = "https://www.techpowerup.com/download/nvidia-dlss-dll/"
+        response = requests.get(url).text
+        soup = BeautifulSoup(response, "lxml")
 
-    # Find the <form> element
-    form = soup.find("form", {"class": "download-version-form"})
-
-    # Find the <input> element within the <form> by name
-    input_element = form.find("input", {"name": "id"})
-
-    # Get the value attribute of the <input> element
-    value = input_element["value"]
-
-    return value
-
-if __name__=="__main__":
-   print( get_file_id(url))
+        dll = soup.find("div", {"class": "version"})
+        file_name = dll.find("div", {"class": "filename"})
+        file_name = file_name.text
+        id = dll.find("input", {"name": "id"})
+        id = id["value"]
+        return id, file_name
+    except Exception as e:
+        print(f"Failed to scrape {url}:{e}")
+        exit()
